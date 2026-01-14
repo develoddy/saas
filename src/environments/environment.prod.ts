0ff59e5ae2a05785e6d158206a758b951245c71c
Eddy Lujan <eddylujann@gmail.com>
2026-01-14 23:06:50 +0100

    💾 Pre-Deploy commit 2026-01-14 23:06:50

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,8 @@ export const environment = {
   API_URL: 'https://api.lujandev.com/api',
   ADMIN_URL: 'https://admin.lujandev.com',
   TIENDA_URL: 'https://tienda.lujandev.com',
-  APP_URL: 'https://app.lujandev.com'
+  APP_URL: 'https://app.lujandev.com',
+  
+  // Stripe PRODUCCIÓN (misma cuenta que merch)
+  stripePublicKey: 'pk_live_51PtUUBChg0ofSaFLNlrWqGCFgjdIshjSZNgx8QHEyJw6iXayFEeRog9yA74eSplm67AmDdt4Li2qsU0trAjcdkNo00lETaDpUZ'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@ export const environment = {
   API_URL: 'http://localhost:3500/api',
   ADMIN_URL: 'http://localhost:4201',
   TIENDA_URL: 'http://localhost:4200',
-  APP_URL: 'http://localhost:4202'
+  APP_URL: 'http://localhost:4202',
+  
+  // Stripe TEST (para desarrollo) 
+  stripePublicKey: 'pk_test_51RkV1QCtfffoVXXrTORpzxQKnNHLvLX09uwlHfyPB242r5UY2VWSI1TxO1AUPxeADN6A6Xghp2EC1y8ijSD6sKeg00qO0iW7Ab'
 };
